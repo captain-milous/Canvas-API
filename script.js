838843cdb949325315f97e39a0439815626136c5
captain-milous <milda.tesar3@gmail.com>
2023-01-26 08:41:24 +0100

    delete button

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let height = Math.ceil(innerHeight) - 130;
 document.getElementById("canvas").style.height = height+"px";
+
+document.getElementById('reset').addEventListener('click', function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}, false);
